8d53c12009bc669cf8776362e2b5d63a5d9ada06
Matheus de Paula Ribeiro <matheusprfh098@gmail.com>
2024-08-12 23:43:28 -0300

    Exercicio 02 Finalizado

---
+++ POOExercicios01/ExerciciosPOO01/src/Exercicio02/ContaBancaria.java
@@ -0,0 +1,40 @@
+package Exercicio02;
+
+public class ContaBancaria {
+
+    private int numeroConta;
+    private double saldo;
+
+    public ContaBancaria() {
+    }
+
+    public ContaBancaria(int numeroConta, double saldo) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public void getSaldo() {
+        System.out.println(saldo);
+    }
+
+    public void depositar(double saldo) {
+        this.saldo = this.saldo + saldo;
+    }
+
+    public void sacar(double saldo) {
+        this.saldo = this.saldo - saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "ContaBancaria{" +
+                "numeroConta=" + numeroConta +
+                ", saldo=" + saldo +
+                '}';
+    }
+}
--- POOExercicios01/ExerciciosPOO01/src/Main.java
+++ POOExercicios01/ExerciciosPOO01/src/Main.java
@@ -1,4 +1,5 @@
 import Exercicio01.Pessoa;
+import Exercicio02.ContaBancaria;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,10 +14,10 @@ public class Main {
         System.out.println(pessoa);
 
         // Exercício 2:
-        // 1. Criar uma instância da classe ContaBancaria com um saldo inicial.
-        // 2. Depositar um valor na conta.
-        // 3. Sacar um valor da conta.
-        // 4. Exibir o saldo final.
+        ContaBancaria conta = new ContaBancaria();
+        conta.depositar(2000);
+        conta.sacar(1987);
+        conta.getSaldo();
 
         // Exercício 3:
         // 1. Criar uma instância da classe Produto.
