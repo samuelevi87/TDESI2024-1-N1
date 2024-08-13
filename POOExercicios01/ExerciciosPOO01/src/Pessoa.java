22101d81503b29fb06ae008f65bafd6d362ae281
PAULO RICARDO DA SILVA VIEIRA NUNES <paulo_rs_nunes@estudante.sesisenai.org.br>
2024-08-12 21:38:34 -0300

    Exercicio 01 Finalizado

---
+++ POOExercicios01/ExerciciosPOO01/src/Pessoa.java
@@ -0,0 +1,32 @@
+public class Pessoa {
+
+    String nome;
+    int idade;
+
+    public Pessoa() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
