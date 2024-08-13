65e27dc4ccad154f0537363d19ca64fb8d26d585
STEPHANIE LOHANA NEUWIEM <stephanie_neuwiem@estudante.sesisenai.org.br>
2024-08-12 21:39:53 -0300

    exercicio 2

--- POOExercicios01/ExerciciosPOO01/src/Exercicio02/ContaBancaria.java
+++ POOExercicios01/ExerciciosPOO01/src/Exercicio02/ContaBancaria.java
@@ -1,2 +1,27 @@
-package Exercicio02;public class ContaBancaria {
+package Exercicio02;
+
+public class ContaBancaria {
+    private int numeroConta;
+    private double saldo;
+
+    public ContaBancaria(int numeroConta, double saldo) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void Depositar (double valor) {
+        this.saldo += valor;
+    }
+
+    public void Sacar (double valor) {
+        this.saldo -= valor;
+    }
 }
--- POOExercicios01/ExerciciosPOO01/src/Main.java
+++ POOExercicios01/ExerciciosPOO01/src/Main.java
@@ -1,4 +1,5 @@
 import Exercicio01.Pessoa;
+import Exercicio02.ContaBancaria;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,16 +7,27 @@ public class Main {
         // 1. Criar uma instância da classe Pessoa.
         // 2. Exibir o nome e a idade da pessoa.
         // 3. Alterar a idade da pessoa e exibir novamente.
-        Pessoa pessoa01 = new Pessoa("Stephanie", 22);
-        System.out.println(pessoa01.getNome() + " tem " + pessoa01.getIdade() + " anos.");
-        pessoa01.setIdade(25);
-        System.out.println(pessoa01.getNome() + " tem " + pessoa01.getIdade() + " anos.");
+
+        //Pessoa pessoa01 = new Pessoa("Stephanie", 22);
+        //System.out.println(pessoa01.getNome() + " tem " + pessoa01.getIdade() + " anos.");
+        //pessoa01.setIdade(25);
+        //System.out.println(pessoa01.getNome() + " tem " + pessoa01.getIdade() + " anos.");
 
         // Exercício 2:
         // 1. Criar uma instância da classe ContaBancaria com um saldo inicial.
         // 2. Depositar um valor na conta.
         // 3. Sacar um valor da conta.
         // 4. Exibir o saldo final.
+
+        ContaBancaria contateste = new ContaBancaria(30091650, 1000);
+        System.out.println("Conta: " + contateste.getNumeroConta());
+        System.out.println("Saldo atual: " + contateste.getSaldo());
+
+        contateste.Depositar(100);
+        System.out.println("Saldo atual: " + contateste.getSaldo());
+
+        contateste.Sacar(500);
+        System.out.println("Saldo atual: " + contateste.getSaldo());
 
         // Exercício 3:
         // 1. Criar uma instância da classe Produto.
