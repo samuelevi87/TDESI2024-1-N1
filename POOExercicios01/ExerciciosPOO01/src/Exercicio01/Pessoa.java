0b10e5207f936a23e04495f40a58d348373d7436
GIANLUCA GRIGNANI <gianluca_grignani@estudante.sesisenai.org.br>
2024-08-12 21:31:44 -0300

    Exercício 01 finalizado

---
+++ POOExercicios01/ExerciciosPOO01/src/Exercicio01/Pessoa.java
@@ -0,0 +1,34 @@
+package Exercicio01;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
--- POOExercicios01/ExerciciosPOO01/src/Main.java
+++ POOExercicios01/ExerciciosPOO01/src/Main.java
@@ -1,9 +1,16 @@
+import Exercicio01.Pessoa;
+
 public class Main {
     public static void main(String[] args) {
         // Exercício 1:
         // 1. Criar uma instância da classe Pessoa.
         // 2. Exibir o nome e a idade da pessoa.
         // 3. Alterar a idade da pessoa e exibir novamente.
+        Pessoa pessoa1 = new Pessoa("Pedro", 20);
+        System.out.println("O nome é: " + pessoa1.getNome() + " e a idade é: " + pessoa1.getIdade());
+        pessoa1.setIdade(50);
+        System.out.println("O nome é: " + pessoa1.getNome() + " e a idade é: " + pessoa1.getIdade());
+
 
         // Exercício 2:
         // 1. Criar uma instância da classe ContaBancaria com um saldo inicial.
