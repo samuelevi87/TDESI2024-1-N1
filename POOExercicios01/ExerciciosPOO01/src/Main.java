e15bd942876d129a2c956ac936bf826061038f93
GABRIELLA MAUREA BENTO <gabriella_bento@estudante.sesisenai.org.br>
2024-08-14 19:51:00 -0300

    Exercício 6

--- POOExercicios01/ExerciciosPOO01/src/Main.java
+++ POOExercicios01/ExerciciosPOO01/src/Main.java
@@ -3,6 +3,7 @@ import Exercicio02.ContaBancaria;
 import Exercicio03.Produto;
 import Exercicio04.Livro;
 import Exercicio05.Carro;
+import Exercicio06.Cliente;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,56 +11,66 @@ public class Main {
         // 1. Criar uma instância da classe Pessoa.
         // 2. Exibir o nome e a idade da pessoa.
         // 3. Alterar a idade da pessoa e exibir novamente.
-        Pessoa pessoa = new Pessoa("Gabriella", 18);
-        System.out.println("Nome: " + pessoa.getNome() + "\nIdade: " + pessoa.getIdade());
-        pessoa.setIdade(19);
-        System.out.println("Idade: " + pessoa.getIdade());
+
+//        Pessoa pessoa = new Pessoa("Gabriella", 18);
+//        System.out.println("Nome: " + pessoa.getNome() + "\nIdade: " + pessoa.getIdade());
+//        pessoa.setIdade(19);
+//        System.out.println("Idade: " + pessoa.getIdade());
 
         // Exercício 2:
         // 1. Criar uma instância da classe ContaBancaria com um saldo inicial.
         // 2. Depositar um valor na conta.
         // 3. Sacar um valor da conta.
         // 4. Exibir o saldo final.
-        ContaBancaria conta = new ContaBancaria(1, 5000.0);
-        conta.depositar(1000.0);
-        conta.sacar(500.0);
-        System.out.println("Saldo final: " + conta.getSaldo());
+
+//        ContaBancaria conta = new ContaBancaria(1, 5000.0);
+//        conta.depositar(1000.0);
+//        conta.sacar(500.0);
+//        System.out.println("Saldo final: " + conta.getSaldo());
 
         // Exercício 3:
         // 1. Criar uma instância da classe Produto.
         // 2. Exibir o nome, preço e quantidade em estoque.
         // 3. Adicionar itens ao estoque e exibir a nova quantidade.
         // 4. Remover itens do estoque e exibir a nova quantidade.
-        Produto produto = new Produto("Cadeira", 10.0, 10);
-        System.out.println(produto);
-        produto.adicionarEstoque(10);
-        System.out.println(produto.getQuantidadeEmEstoque());
-        produto.removerEstoque(9);
-        System.out.println(produto.getQuantidadeEmEstoque());
+
+//        Produto produto = new Produto("Cadeira", 10.0, 10);
+//        System.out.println(produto);
+//        produto.adicionarEstoque(10);
+//        System.out.println(produto.getQuantidadeEmEstoque());
+//        produto.removerEstoque(9);
+//        System.out.println(produto.getQuantidadeEmEstoque());
 
         // Exercício 4:
         // 1. Criar uma instância da classe Livro.
         // 2. Exibir o título, autor e ano de publicação.
         // 3. Alterar o ano de publicação e exibir novamente.
-        Livro livro = new Livro("A vida invisível de Addie Larue", "V.E Schwab", 2024);
-        System.out.println("Título: " + livro.getTitulo() + "\nAutor: " + livro.getAutor() + "\nAno: " + livro.getAnoPublicacao());
-        livro.setAnoPublicacao(2023);
-        System.out.println("Ano: " + livro.getAnoPublicacao());
+
+//        Livro livro = new Livro("A vida invisível de Addie Larue", "V.E Schwab", 2024);
+//        System.out.println("Título: " + livro.getTitulo() + "\nAutor: " + livro.getAutor() + "\nAno: " + livro.getAnoPublicacao());
+//        livro.setAnoPublicacao(2023);
+//        System.out.println("Ano: " + livro.getAnoPublicacao());
 
         // Exercício 5:
         // 1. Criar uma instância da classe Carro.
         // 2. Exibir a marca, modelo, ano e quilometragem do carro.
         // 3. Adicionar quilometragem ao carro e exibir a nova quilometragem.
-        Carro carro = new Carro("Fiat", "Uno", 2024, 20);
-        System.out.println("Marca: " + carro.getMarca() + ", modelo: " + carro.getModelo() + ", ano: " + carro.getAno() + ", quilometragem: " + carro.getQuilometragem());
-        carro.adicionarQuilometragem(30);
-        System.out.println(carro.getQuilometragem());
+
+//        Carro carro = new Carro("Fiat", "Uno", 2024, 20);
+//        System.out.println("Marca: " + carro.getMarca() + ", modelo: " + carro.getModelo() + ", ano: " + carro.getAno() + ", quilometragem: " + carro.getQuilometragem());
+//        carro.adicionarQuilometragem(30);
+//        System.out.println(carro.getQuilometragem());
 
 
         // Exercício 6:
         // 1. Criar uma instância da classe Cliente.
         // 2. Exibir o nome, email e telefone do cliente.
         // 3. Alterar o telefone do cliente e exibir novamente.
+
+        Cliente cliente = new Cliente("Gabriella", "gabi@gmail.com", "984292863");
+        System.out.println(cliente.getNome() + " " + cliente.getEmail() + " " + cliente.getTelefone());
+        cliente.setTelefone("1234");
+        System.out.println(cliente.getTelefone());
 
         // Exercício 7:
         // 1. Criar uma instância da classe Pedido.
