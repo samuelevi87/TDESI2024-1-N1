8d9e85008615bdc3e4639fd6b579713c77430cdf
Bryan Rory <bryanrory13@gmail.com>
2024-08-20 18:50:38 -0300

    Exe3

---
+++ POOExercicios01/ExerciciosPOO01/src/Exercicio03/Produto.java
@@ -0,0 +1,33 @@
+package Exercicio03;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidadeEmEstoque;
+
+    public Produto(String nome, double preco, int quantidadeEmEstoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void adicionarEstoque(int quantidade) {
+        this.quantidadeEmEstoque += quantidade;
+    }
+
+    public void removerEstoque(int quantidade) {
+        this.quantidadeEmEstoque -= quantidade;
+    }
+}
--- POOExercicios01/ExerciciosPOO01/src/Exercicio04/Livro.java
+++ POOExercicios01/ExerciciosPOO01/src/Exercicio04/Livro.java
@@ -1,2 +1,29 @@
-package Exercicio04;public class Livro {
+package Exercicio04;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
 }
