cb7f2d31507fd5259676b8ccaca99d4295c7f69d
MOISES VIANNA VANTI <moises_vanti@estudante.sc.senai.br>
2024-08-26 19:29:36 -0300

    Exercicio011

---
+++ POOExercicios01/ExerciciosPOO01/src/Exercicio11/Fornecedor.java
@@ -0,0 +1,30 @@
+
+package Exercicio11;
+
+public class Fornecedor {
+    String nome;
+    String cnpj;
+    String telefone;
+
+    public Fornecedor(String nome, String cnpj, String telefone) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
