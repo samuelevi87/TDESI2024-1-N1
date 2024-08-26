a66616f7edb6776c269e658c793de6e4253a3aed
VICTOR RIAN RAMOS DE SOUZA <victor_r_souza@estudante.sesisenai.org.br>
2024-08-26 19:43:14 -0300

    Exercicio 8

---
+++ POOExercicios01/ExerciciosPOO01/src/Exercicio08/Funcionario.java
@@ -0,0 +1,30 @@
+
+package Exercicio08;
+
+public class Funcionario {
+    String nome;
+    String cargo;
+    double salario;
+
+    public Funcionario(String nome, String cargo, double salario) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
