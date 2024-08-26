42be9752fa2c4e2eee8d7e7a12bcf5c2aa1c7c61
VICTOR RIAN RAMOS DE SOUZA <victor_r_souza@estudante.sesisenai.org.br>
2024-08-26 19:54:45 -0300

    Exercicio 13

---
+++ POOExercicios01/ExerciciosPOO01/src/Exercicio13/Veiculoempresa.java
@@ -0,0 +1,36 @@
+
+package Exercicio13;
+
+public class VeiculoEmpresa {
+    String placa;
+    String modelo;
+    int ano;
+    double quilometragem;
+
+    public VeiculoEmpresa(String placa, String modelo, int ano, double quilometragem) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.quilometragem = quilometragem;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public double getQuilometragem() {
+        return quilometragem;
+    }
+
+    public void adicionarQuilometragem(double km) {
+        quilometragem += km;
+    }
+}
