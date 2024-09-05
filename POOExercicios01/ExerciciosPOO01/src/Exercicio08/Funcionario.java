9750a205f8482ed1e63c6286cf694ec22ce1c0a7
GIOVANI LENOIR <giovani_lenoir@estudante.sesisenai.org.br>
2024-09-05 19:17:19 -0300

    Exercicio08

---
+++ POOExercicios01/ExerciciosPOO01/src/Exercicio08/Funcionario.java
@@ -0,0 +1,29 @@
+package Exercicio08;
+
+public class Funcionario {
+    private String nome;
+    private String cargo;
+    private double salario;
+
+    public Funcionario(String nome, String cargo, double salario) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
