bfa5cf6450eddb55c4988d5861c9ae9a176bf5b6
GABRIELLA MAUREA BENTO <gabriella_bento@estudante.sesisenai.org.br>
2024-09-16 20:51:23 -0300

    Demonstração de polimorfismo com diferentes sons de animais

---
+++ Heranca-Polimorfismo-Java/src/main/java/Cachorro.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class Cachorro extends Animal{
+    public Cachorro(String nome, int idade) {
+        super(nome, idade);
+    }
+    @Override
+    public void emitirSom() {
+        System.out.println("au au au au au au au au au au au au au au au au au au au au au au au au au au au au au au auuuuuu");
+    }
+}
--- Heranca-Polimorfismo-Java/src/main/java/Main.java
+++ Heranca-Polimorfismo-Java/src/main/java/Main.java
@@ -1,16 +1,20 @@
 package main.java;
 
 public class Main {
-    //FIXME Adicione o método main usando o atalho psvm
-
     public static void main(String[] args) {
-        Animal leao = new Animal("Alex", 5);
-        Animal cachorro = new Animal("lola", 8);
-        Animal gato = new Animal("Moacir", 5);
+        Animal leao = new Leao("Alex", 5);
+        Animal cachorro = new Cachorro("Lola", 8);
+        Animal elefante = new Elefante("Bernado", 10);
+        Animal gato = new Gato("Yoon", 4);
+        Animal guaxinim = new Guaxinim("Rocket", 1);
 
         leao.emitirSom();
+        cachorro.emitirSom();
+        elefante.emitirSom();
+        gato.emitirSom();
+        guaxinim.emitirSom();
+
     }
 
-    //TODO O conteúdo será desenvolvido durante a aula
 }
 
