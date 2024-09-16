f27a92fe5abae3e860a58a25b5f434af1de3a30f
JONATHAN SOST DOS SANTOS <jonathan_santos11@estudante.sesisenai.org.br>
2024-09-16 19:51:51 -0300

    Implementação de classes Leao e Elefante usando herança

---
+++ Heranca-Polimorfismo-Java/src/main/java/Leao.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class Leao extends Animal {
+    public Leao(String nome, int idade) {
+        super(nome, idade);
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Roarrrr");
+    }
+}
