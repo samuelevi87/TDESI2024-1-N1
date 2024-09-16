536856e3474a85114352703585538dfca7dcc33a
BRYAN RORY FERREIRA <bryan_rory@estudante.sc.senai.br>
2024-09-16 19:53:56 -0300

    Implementação da classe leão e elefante usando herança

--- Heranca-Polimorfismo-Java/src/main/java/Animal.java
+++ Heranca-Polimorfismo-Java/src/main/java/Animal.java
@@ -13,5 +13,4 @@ public class Animal {
     public void emitirSom(){
         System.out.println("Som do animal");
     }
-
 }
--- Heranca-Polimorfismo-Java/src/main/java/Main.java
+++ Heranca-Polimorfismo-Java/src/main/java/Main.java
@@ -1,8 +1,5 @@
 package main.java;
 
-
-
-
 public class Main {
 
     public static void main(String[] args) {
