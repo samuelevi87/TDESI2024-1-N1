38c022cc59e89e1127fc75e196e6439c2271381f
ruan_fortes <ruan_fortes@estudante.sesisenai.org.br>
2024-09-16 19:32:03 -0300

    Animal

--- Heranca-Polimorfismo-Java/src/main/java/Animal.java
+++ Heranca-Polimorfismo-Java/src/main/java/Animal.java
@@ -1,6 +1,25 @@
 package main.java;
 
 public class Animal {
+    private String nome;
+    private int idade;
+
+    public Animal (String nome, int idade){
+        this.nome=nome;
+        this.idade=idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void emetirSOm(){
+        System.out.println("som generico do animal");
+    }
     //TODO Atributos, construtor e métodos básicos serão adicionados durante a aula
 
 }
--- Heranca-Polimorfismo-Java/src/main/java/Main.java
+++ Heranca-Polimorfismo-Java/src/main/java/Main.java
@@ -1,6 +1,12 @@
 package main.java;
 
 public class Main {
+    public static void main(String[] args) {
+        Animal leao = new Animal("Alex",22 );
+        Animal cachorro = new Animal("toto",5);
+        Animal gata = new Animal("mimi",4);
+
+    }
     //FIXME Adicione o método main usando o atalho psvm
 
     //TODO O conteúdo será desenvolvido durante a aula
