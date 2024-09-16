4bfca402a6a950caa5eaa797f0229eff5f741995
TULIO DE LIMA AVELINO <tulio_avelino@estudante.sesisenai.org.br>
2024-09-16 20:02:26 -0300

    criando classes e heranca

--- Heranca-Polimorfismo-Java/src/main/java/Main.java
+++ Heranca-Polimorfismo-Java/src/main/java/Main.java
@@ -1,6 +1,13 @@
 package main.java;
 
 public class Main {
+    public static void main(String[] args) {
+        Animal leao = new main.java.Animal("Alex", 22);
+        Animal cachorro = new main.java.Animal("Toto", 10);
+        Animal elefante = new main.java.Animal("Ganesha", 35);
+
+
+    }
     //FIXME Adicione o método main usando o atalho psvm
 
     //TODO O conteúdo será desenvolvido durante a aula
