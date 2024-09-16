d69ec75cd027fa9df2b26c5b1c7d386dc2b2ad2d
STEPHANIE LOHANA NEUWIEM <stephanie_neuwiem@estudante.sesisenai.org.br>
2024-09-16 19:22:21 -0300

    Criação da classe animal básica

--- Heranca-Polimorfismo-Java/src/main/java/Animal.java
+++ Heranca-Polimorfismo-Java/src/main/java/Animal.java
@@ -2,5 +2,15 @@ package main.java;
 
 public class Animal {
     //TODO Atributos, construtor e métodos básicos serão adicionados durante a aula
+    private String nome;
+    private int idade;
 
+    public Animal(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void emitirSom(){
+        System.out.println("Som do animal");
+    }
 }
