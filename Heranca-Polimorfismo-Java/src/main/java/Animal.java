3253677a2d7303a879b96ce526a994e1e3772365
CLAUDYSON CLAIRMEUS <claudyson_clairmeus@estudante.sesisenai.org.br>
2024-09-16 19:24:55 -0300

    Criacao da classe Animal basica

--- Heranca-Polimorfismo-Java/src/main/java/Animal.java
+++ Heranca-Polimorfismo-Java/src/main/java/Animal.java
@@ -2,5 +2,14 @@ package main.java;
 
 public class Animal {
     //TODO Atributos, construtor e métodos básicos serão adicionados durante a aula
+private String nome;
+private int idade;
 
+    public Animal(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    public void emitirSom(){
+        System.out.println("som generico animal");
+    }
 }
--- Heranca-Polimorfismo-Java/src/main/java/Main.java
+++ Heranca-Polimorfismo-Java/src/main/java/Main.java
@@ -2,6 +2,14 @@ package main.java;
 
 public class Main {
     //FIXME Adicione o método main usando o atalho psvm
+    public static void main(String[] args) {
+        Animal leao = new Animal("Alex",22);
+        Animal cachorro = new Animal("Toto",50);
+        Animal gato = new Animal( "MOmo",4);
 
+        leao.emitirSom();
+        cachorro.emitirSom();
+        gato.emitirSom();
+    }
     //TODO O conteúdo será desenvolvido durante a aula
 }
