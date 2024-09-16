cd96a30058b65fed40d39aeed00bbf67fe70b9d0
JONATHAN SOST DOS SANTOS <jonathan_santos11@estudante.sesisenai.org.br>
2024-09-16 19:22:12 -0300

    Criação da classe Animal básica

--- Heranca-Polimorfismo-Java/src/main/java/Animal.java
+++ Heranca-Polimorfismo-Java/src/main/java/Animal.java
@@ -3,4 +3,18 @@ package main.java;
 public class Animal {
     //TODO Atributos, construtor e métodos básicos serão adicionados durante a aula
 
+    private String nome;
+    private int idade;
+
+    public Animal() {
+    }
+
+    public Animal(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void emitirSom() {
+        System.out.println("Som genérico do animal");
+    }
 }
