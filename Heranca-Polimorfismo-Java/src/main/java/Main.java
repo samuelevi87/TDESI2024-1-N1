88f8f73704ec6ed1bc2310dbc587f3986a405257
GIANLUCA GRIGNANI <gianluca_grignani@estudante.sesisenai.org.br>
2024-09-16 20:50:54 -0300

    Demonstração de polimorfismo com diferentes sons de animais

--- Heranca-Polimorfismo-Java/src/main/java/Main.java
+++ Heranca-Polimorfismo-Java/src/main/java/Main.java
@@ -3,12 +3,16 @@ package main.java;
 public class Main {
     //FIXME Adicione o método main usando o atalho psvm
     public static void main(String[] args) {
-        Animal leao = new Animal("Alex", 22);
-        Animal cachorro = new Animal("Luck",5);
-        Animal gato = new Animal("Moacir", 4);
+        Animal leao = new Leao("Alex", 22);
+        Animal elefante = new Elefante("Moacir", 4);
+        Animal gato = new Gato("Pedro", 3);
+        Animal guaxinim = new Guaxinim("José", 8);
+        Animal cachorro = new Cachorro("Luke", 12);
         leao.emitirSom();
-        cachorro.emitirSom();
+        elefante.emitirSom();
         gato.emitirSom();
+        guaxinim.emitirSom();
+        cachorro.emitirSom();
     }
     //TODO O conteúdo será desenvolvido durante a aula
 
