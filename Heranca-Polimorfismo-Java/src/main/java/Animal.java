39b44940b59bc8f2e525f459e5521f73e674b2c3
taina_e_martins <tainaestefanim@gmail.com>
2024-09-16 19:29:24 -0300

    Criação da classe Animal b♣sica

---
+++ Heranca-Polimorfismo-Java/src/main/java/Animal.java
@@ -0,0 +1,33 @@
+package main.java;
+
+public class Animal {
+    //TODO Atributos, construtor e métodos básicos serão adicionados durante a aula
+
+    private String nome;
+    private int idade;
+
+    public Animal(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void emitirSom() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
