5af3eb33704995cfd98083b47d55e5e29706e80c
MATHEUS DE PAULA RIBEIRO <matheus_p_ribeiro@estudante.sesisenai.org.br>
2024-09-16 19:53:22 -0300

    Implementação de classe Leao e Elefante usando herença

--- Heranca-Polimorfismo-Java/src/main/java/Animal.java
+++ Heranca-Polimorfismo-Java/src/main/java/Animal.java
@@ -5,35 +5,8 @@ public class Animal {
     private String nome;
     private int idade;
 
-    public Animal() {
-    }
-
     public Animal(String nome, int idade) {
         this.nome = nome;
-        this.idade = idade;
-    }
-
-    @Override
-    public String toString() {
-        return "Animal{" +
-                "nome='" + nome + '\'' +
-                ", idade=" + idade +
-                '}';
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
         this.idade = idade;
     }
 
---
+++ Heranca-Polimorfismo-Java/src/main/java/Leao.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class Leao extends Animal {
+
+    public Leao(String nome, int idade) {
+        super(nome, idade);
+    }
+    @Override
+    public void emitirSom(){
+        System.out.println("Roarr");
+    }
+
+}
