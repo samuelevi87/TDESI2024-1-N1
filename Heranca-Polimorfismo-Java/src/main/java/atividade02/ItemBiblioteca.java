b9f65ab6f389afbeda0a5644df95b3ff1ac6cc1f
GianGrig <gianluca_grignani@estudante.sesisenai.org.br>
2024-09-23 15:12:19 -0300

    Atividade 2 atualizada

--- Heranca-Polimorfismo-Java/src/main/java/atividade02/ItemBiblioteca.java
+++ Heranca-Polimorfismo-Java/src/main/java/atividade02/ItemBiblioteca.java
@@ -1,4 +1,65 @@
 package main.java.atividade02;
 
 public class ItemBiblioteca {
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    private boolean disponivel;
+
+    public ItemBiblioteca(String titulo, boolean disponivel, int anoPublicacao, String autor) {
+        this.titulo = titulo;
+        this.disponivel = disponivel;
+        this.anoPublicacao = anoPublicacao;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public boolean getDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public void emprestar(){
+        this.disponivel = false;
+    }
+    public void devolver(){
+        this.disponivel = true;
+    }
+
+    public abstract double calcularMulta(int diasAtraso);
+
+    @Override
+    public String toString() {
+        return  "Livro:\n" +
+                "Titulo: " + titulo +
+                "Autor: " + autor +
+                "Ano de Publicacao: " + anoPublicacao +
+                "Disponibilidade: " + disponivel;
+    }
 }
--- Heranca-Polimorfismo-Java/src/main/java/atividade02/LivroFisico.java
+++ Heranca-Polimorfismo-Java/src/main/java/atividade02/LivroFisico.java
@@ -1,4 +1,11 @@
 package main.java.atividade02;
 
-public class LivroFisico {
+public class LivroFisico extends ItemBiblioteca{
+    private int numeroPaginas;
+
+
+    public LivroFisico(String titulo, boolean disponivel, int anoPublicacao, String autor) {
+        super(titulo, disponivel, anoPublicacao, autor);
+    }
+
 }
