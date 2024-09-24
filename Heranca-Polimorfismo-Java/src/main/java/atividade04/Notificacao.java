b2b2444f278b92be75a0d736afffecf55bd923e8
JONATHAN SOST DOS SANTOS <jonathan_santos11@estudante.sesisenai.org.br>
2024-09-23 21:16:07 -0300

    Criação da classe Notificacao e interface Notificavel para a atividade 04

--- Heranca-Polimorfismo-Java/src/main/java/atividade04/Notificacao.java
+++ Heranca-Polimorfismo-Java/src/main/java/atividade04/Notificacao.java
@@ -1,2 +1,48 @@
-package main.java.atividade04;public class Notificacao {
+package main.java.atividade04;
+
+import java.time.LocalDateTime;
+
+public abstract class Notificacao implements Notificavel{
+    private String mensagem;
+    private String destinatario;
+    private LocalDateTime dataEnvio;
+
+    public Notificacao(String mensagem, String destinatario) {
+        this.mensagem = mensagem;
+        this.destinatario = destinatario;
+        this.dataEnvio = LocalDateTime.now();
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public LocalDateTime getDataEnvio() {
+        return dataEnvio;
+    }
+
+    @Override
+    public String toString() {
+        return "\nMensagem: \"" + mensagem + '\"' +
+                "\nDestinatario: \"" + destinatario + '\"' +
+                "\nData de envio: " + dataEnvio;
+    }
+
+    /**
+     * Formata a mensagem da notificação.
+     * @return A mensagem formatada.
+     */
+    public abstract String formatarMensagem();
 }
--- Heranca-Polimorfismo-Java/src/main/java/atividade04/Notificavel.java
+++ Heranca-Polimorfismo-Java/src/main/java/atividade04/Notificavel.java
@@ -1,2 +1,11 @@
-package main.java.atividade04;public interface Notificavel {
+package main.java.atividade04;
+
+/**
+ * Interface que define o comportamento para envio de notificação
+ */
+public interface Notificavel {
+    /**
+     * Envia a notificação
+     */
+    public void enviarNotificacao();
 }
