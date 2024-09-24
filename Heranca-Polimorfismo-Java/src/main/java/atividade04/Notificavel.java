3f86ca03db4f6779db3650fb14891d926aec11bc
CLAUDYSON CLAIRMEUS <claudyson_clairmeus@estudante.sesisenai.org.br>
2024-09-23 21:16:12 -0300

    como fazer Interface no java

---
+++ Heranca-Polimorfismo-Java/src/main/java/atividade04/Notificavel.java
@@ -0,0 +1,11 @@
+package main.java.atividade04;
+/**
+ * interfce que define o comportamento para envio de notificacao
+ */
+
+public interface Notificavel {
+    /**
+     * Envia a notificacao
+     */
+    public void enviarNotificacao();
+}
