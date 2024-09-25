af299c9f8a3a82837a98519474541b011a8380f9
GABRIELLA MAUREA BENTO <gabriella_bento@estudante.sesisenai.org.br>
2024-09-24 21:50:07 -0300

    Atividade 04: alteração classe GerenciadorNotificacoes e instância na Main

--- Heranca-Polimorfismo-Java/src/main/java/Main.java
+++ Heranca-Polimorfismo-Java/src/main/java/Main.java
@@ -10,51 +10,63 @@ import main.java.atividade03.Caminhao;
 import main.java.atividade03.Carro;
 import main.java.atividade03.Frota;
 import main.java.atividade03.Moto;
+import main.java.atividade04.EmailNotificacao;
+import main.java.atividade04.GerenciadorNotificacoes;
+import main.java.atividade04.PushNotificacao;
+import main.java.atividade04.SMSNotificacao;
 
 public class Main {
     public static void main(String[] args) {
-//        Empresa empresaDeTeste = new Empresa();
-//
-//        empresaDeTeste.adicionarFuncionario(new Desenvolvedor("Gabriella", 1, 15000.0, "Java"));
-//        empresaDeTeste.adicionarFuncionario(new Desenvolvedor("Taina", 2, 20000.0, "JavaScript"));
-//        empresaDeTeste.adicionarFuncionario(new Gerente("Nathalia", 3, 10000.0, 10000));
-//        empresaDeTeste.adicionarFuncionario(new Gerente("Maria", 4,12000.0, 5000));
-//        empresaDeTeste.adicionarFuncionario(new AnalistaQA("Sayonara", 5, 14000, 200));
-//        empresaDeTeste.adicionarFuncionario(new AnalistaQA("Juliana", 6, 50000, 5));
-//
-//        empresaDeTeste.listarFuncionarios();
-//        empresaDeTeste.listarFuncionarios02();
-//        System.out.println(empresaDeTeste.calcularFolhaDePagamentoTotal());
-//        empresaDeTeste.listarFuncionarios02();
+//        atividade01();
+//        atividade02();
+//        atividade03();
+        atividade04();
+    }
+    public static void atividade01(){
+        Empresa empresaDeTeste = new Empresa();
 
+        empresaDeTeste.adicionarFuncionario(new Desenvolvedor("Gabriella", 1, 15000.0, "Java"));
+        empresaDeTeste.adicionarFuncionario(new Desenvolvedor("Taina", 2, 20000.0, "JavaScript"));
+        empresaDeTeste.adicionarFuncionario(new Gerente("Nathalia", 3, 10000.0, 10000));
+        empresaDeTeste.adicionarFuncionario(new Gerente("Maria", 4,12000.0, 5000));
+        empresaDeTeste.adicionarFuncionario(new AnalistaQA("Sayonara", 5, 14000, 200));
+        empresaDeTeste.adicionarFuncionario(new AnalistaQA("Juliana", 6, 50000, 5));
 
-//        Biblioteca biblioteca = new Biblioteca();
-//
-//        biblioteca.adicionarItensABiblioteca(new LivroFisico("A vida invisivel de Addie Larue", "V.E Schwab", 2020, 620));
-//        biblioteca.adicionarItensABiblioteca(new LivroFisico("Bem-vindos a livraria Hyunam Dong", "Hwang Boreum", 2021,230));
-//
-//        biblioteca.adicionarItensABiblioteca(new AudioLivro("Os sete maridos de Evelyn Hugo", "Taylor Jenkins Reid", 2023, 1000));
-//        biblioteca.adicionarItensABiblioteca(new AudioLivro("Daisy Jones & The Six", "Taylor Jenkins Reid", 2024, 1500));
-//
-//        biblioteca.adicionarItensABiblioteca(new Ebook("Loucos por livros", "Emily Henry", 2024,1.5));
-//        biblioteca.adicionarItensABiblioteca(new Ebook("Leitura de Verão", "Emily Henry", 2024, 2.1));
-//
-//        biblioteca.listarItens();
-//        System.out.println();
-//        biblioteca.emprestarItem("Os sete maridos de Evelyn Hugo");
-//        System.out.println(biblioteca.calcularMulta("Os sete maridos de evelyn hugo", 10));
-//        biblioteca.listarItens();
-//        System.out.println();
-//        biblioteca.devolverItem("Leitura de verão");
-//        biblioteca.devolverItem("Os sete maridos de Evelyn Hugo");
-//        biblioteca.listarItens();
-//        System.out.println();
-//        biblioteca.emprestarItem("A vida invisivel de Addie Larue");
-//        System.out.println(biblioteca.calcularMulta("A vida invisivel de Addie Larue", 10));
-//        biblioteca.emprestarItem("Loucos por livros");
-//        System.out.println(biblioteca.calcularMulta("Loucos por livros", 10));
+        empresaDeTeste.listarFuncionarios();
+        empresaDeTeste.listarFuncionarios02();
+        System.out.println(empresaDeTeste.calcularFolhaDePagamentoTotal());
+        empresaDeTeste.listarFuncionarios02();
 
+    }
+    public static void atividade02(){
+        Biblioteca biblioteca = new Biblioteca();
 
+        biblioteca.adicionarItensABiblioteca(new LivroFisico("A vida invisivel de Addie Larue", "V.E Schwab", 2020, 620));
+        biblioteca.adicionarItensABiblioteca(new LivroFisico("Bem-vindos a livraria Hyunam Dong", "Hwang Boreum", 2021,230));
+
+        biblioteca.adicionarItensABiblioteca(new AudioLivro("Os sete maridos de Evelyn Hugo", "Taylor Jenkins Reid", 2023, 1000));
+        biblioteca.adicionarItensABiblioteca(new AudioLivro("Daisy Jones & The Six", "Taylor Jenkins Reid", 2024, 1500));
+
+        biblioteca.adicionarItensABiblioteca(new Ebook("Loucos por livros", "Emily Henry", 2024,1.5));
+        biblioteca.adicionarItensABiblioteca(new Ebook("Leitura de Verão", "Emily Henry", 2024, 2.1));
+
+        biblioteca.listarItens();
+        System.out.println();
+        biblioteca.emprestarItem("Os sete maridos de Evelyn Hugo");
+        System.out.println(biblioteca.calcularMulta("Os sete maridos de evelyn hugo", 10));
+        biblioteca.listarItens();
+        System.out.println();
+        biblioteca.devolverItem("Leitura de verão");
+        biblioteca.devolverItem("Os sete maridos de Evelyn Hugo");
+        biblioteca.listarItens();
+        System.out.println();
+        biblioteca.emprestarItem("A vida invisivel de Addie Larue");
+        System.out.println(biblioteca.calcularMulta("A vida invisivel de Addie Larue", 10));
+        biblioteca.emprestarItem("Loucos por livros");
+        System.out.println(biblioteca.calcularMulta("Loucos por livros", 10));
+
+    }
+    public static void atividade03(){
         Frota frota = new Frota();
 
         frota.adicionarVeiculo(new Carro("1234", "fiat", 2006, 10000.0, 4));
@@ -71,6 +83,18 @@ public class Main {
         frota.removerVeiculo("1234");
         frota.listarFrota();
         System.out.println(frota.calcularCustoTotalViagem(10));
+    }
+    public static void atividade04(){
+        GerenciadorNotificacoes gerenciador = new GerenciadorNotificacoes();
+
+        gerenciador.adicionarNotificacao(new EmailNotificacao("Sua senha foi alterada", "gabimabento@gmail.com", "Alteração senha"));
+        gerenciador.adicionarNotificacao(new EmailNotificacao("Boa tarde", "gabi@gmail.com", "Saudações"));
+
+        gerenciador.adicionarNotificacao(new PushNotificacao("Alerta", "Gabriella", "Redmi 10s"));
+        gerenciador.adicionarNotificacao(new PushNotificacao("Dispositivo superaquecido", "Taina", "Samsung galaxy"));
+
+        gerenciador.adicionarNotificacao(new SMSNotificacao("Renove seu plano", "Gabriella", "47984292863"));
+        gerenciador.adicionarNotificacao(new SMSNotificacao("voce tem recado", "Gabriella", "564864564"));
     }
 
 }
--- Heranca-Polimorfismo-Java/src/main/java/atividade04/GerenciadorNotificacoes.java
+++ Heranca-Polimorfismo-Java/src/main/java/atividade04/GerenciadorNotificacoes.java
@@ -5,21 +5,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GerenciadorNotificacoes {
-    private List<Notificacao> notificacoes;
+    private List<Notificacao> notificacoesPendentes;
+    private List<Notificacao> notificacoesEnviadas;
+
 
     public GerenciadorNotificacoes() {
-        notificacoes = new ArrayList<>();
+        this.notificacoesPendentes = new ArrayList<>();
+        this.notificacoesEnviadas = new ArrayList<>();
     }
     public void adicionarNotificacao(Notificacao notificacao){
-        notificacoes.add(notificacao);
+        notificacoesPendentes.add(notificacao);
     }
     public void enviarNotificacoesPendentes(){
-        notificacoes.stream().filter(notificacao -> notificacao.getDataEnvio().isBefore(LocalDateTime.now()) ||
-                        notificacao.getDataEnvio().isEqual(LocalDateTime.now()))
-                .forEach(Notificacao::enviarNotificacao);
+        notificacoesPendentes.forEach(notificacao -> {
+            notificacao.enviarNotificacao();
+            notificacoesEnviadas.add(notificacao);
+        });
+        notificacoesPendentes.clear();
     }
     public void listarNotificacoesEnviadas(){
-        notificacoes.stream().filter((notificacao -> notificacao.getDataEnvio().isBefore(LocalDateTime.now())))
-                .forEach(System.out::println);
+        notificacoesEnviadas.forEach(System.out::println);
     }
 }
