5bab3c53728b344752d783a912a2793c548f6e0e
Jonathan Sost <jonathan_santos11@estudante.sesisenai.org.br>
2024-09-30 23:36:40 -0300

    Implementada classe Hotel conforme atividade 09

---
+++ Heranca-Polimorfismo-Java/src/main/java/atividade09/Hotel.java
@@ -0,0 +1,33 @@
+package main.java.atividade09;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Hotel {
+    private List<Acomodacao> listaDeAcomodacoes;
+
+    public Hotel() {
+        this.listaDeAcomodacoes = new ArrayList<>();
+    }
+
+    public void adicionarAcomodacoes(Acomodacao acomodacao) {
+        this.listaDeAcomodacoes.add(acomodacao);
+    }
+
+    public void listarAcomodacoesDisponiveis() {
+        listaDeAcomodacoes.stream().filter(Acomodacao::getDisponivel).toList().forEach(System.out::println);
+    }
+
+    public void reservarAcomodacaoEspecifica(Integer numero) {
+        listaDeAcomodacoes.stream().filter(acomodacao -> acomodacao.getNumero().equals(numero)).findFirst().ifPresent(Acomodacao::reservar);
+    }
+
+    public void liberarAcomodacaoEspecifica(Integer numero) {
+        listaDeAcomodacoes.stream().filter(acomodacao -> acomodacao.getNumero().equals(numero)).findFirst().ifPresent(Acomodacao::liberar);
+    }
+
+    public Double calcularReceitaTotal(Integer numeroNoites) {
+        return listaDeAcomodacoes.stream().mapToDouble(acomodacao -> acomodacao.calcularPrecoTotal(numeroNoites)).sum();
+    }
+}
