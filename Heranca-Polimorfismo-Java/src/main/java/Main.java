39d8fdc3b28785cf9b1652b6c0f037be8a2f3349
Jonathan Sost <jonathan_santos11@estudante.sesisenai.org.br>
2024-10-01 14:46:44 -0300

    Atualizadas as classes Empresa e Main conforme atividade 10

--- Heranca-Polimorfismo-Java/src/main/java/Main.java
+++ Heranca-Polimorfismo-Java/src/main/java/Main.java
@@ -1,49 +1,38 @@
 package main.java;
 
-import main.java.atividade05.Boleto;
-import main.java.atividade05.CartaoCredito;
-import main.java.atividade05.ProcessadorPagamento;
-import main.java.atividade05.TransferenciaBancaria;
-import main.java.atividade09.*;
+import main.java.atividade10.*;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        ProcessadorPagamento processadorPagamento = new ProcessadorPagamento();
-        processadorPagamento.adicionarPagamentos(new Boleto("teste 1", 100.0, "123456789"));
-        processadorPagamento.adicionarPagamentos(new CartaoCredito("teste 2", 250.0, "123456789012345", "MasterCard"));
-        processadorPagamento.adicionarPagamentos(new TransferenciaBancaria("teste 3", 570.0, "Itaú", "01234", "0000-1"));
+        Empresa empresa = new Empresa();
+        Departamento departamento1 = new Departamento();
+        Departamento departamento2 = new Departamento();
+        Departamento departamento3 = new Departamento();
 
-        RedeHoteis redeDeHoteis = new RedeHoteis();
-        Hotel hotel1 = new Hotel();
-        Hotel hotel2 = new Hotel();
-        Hotel hotel3 = new Hotel();
+        departamento1.adicionarFuncionario(new Analista("Jose", "1283671", LocalDate.now().minusYears(3), 2000.0, "teste", Analista.NivelSenioridade.JUNIOR));
+        departamento1.adicionarFuncionario(new Gerente("Alberto", "345623467", LocalDate.now().minusYears(7), 5000.0, "teste", 500.0));
+        departamento1.adicionarFuncionario(new Vendedor("Ronaldo", "34673247234", LocalDate.now().minusYears(4), 2500.0, 50.0, 5000.0));
 
-        hotel1.adicionarAcomodacoes(new QuartoLuxo(1234, 4, 200.0, true, true));
-        hotel2.adicionarAcomodacoes(new QuartoLuxo(4321, 3, 250.0, false, true));
-        hotel3.adicionarAcomodacoes(new QuartoLuxo(2143, 4, 175.0, false, false));
-        hotel1.adicionarAcomodacoes(new QuartoStandard(9823, 4, 300.0, QuartoStandard.TipoToCama.CASAL));
-        hotel2.adicionarAcomodacoes(new QuartoStandard(7462, 3, 350.0, QuartoStandard.TipoToCama.CASAL));
-        hotel3.adicionarAcomodacoes(new QuartoStandard(5726, 4, 275.0, QuartoStandard.TipoToCama.SOLTEIRO));
-        hotel1.adicionarAcomodacoes(new Suite(9283, 4, 400.0, 4, true));
-        hotel2.adicionarAcomodacoes(new Suite(2748, 4, 425.0, 3, false));
-        hotel3.adicionarAcomodacoes(new Suite(5837, 4, 450.0, 2, false));
-        redeDeHoteis.adicionarHotel(hotel1);
-        redeDeHoteis.adicionarHotel(hotel2);
-        redeDeHoteis.adicionarHotel(hotel3);
+        departamento2.adicionarFuncionario(new Analista("Josevaldo", "38274215", LocalDate.now().minusYears(1), 3000.0, "teste", Analista.NivelSenioridade.PLENO));
+        departamento2.adicionarFuncionario(new Gerente("Derivaldo", "27582124", LocalDate.now().minusYears(15), 10000.0, "teste", 1000.0));
+        departamento2.adicionarFuncionario(new Vendedor("Jubiscreudo", "587283472", LocalDate.now().minusYears(6), 3000.0, 100.0, 8000.0));
 
-        System.out.println("===== Acomodações disponíveis no hotel 1 =====");
-        hotel1.listarAcomodacoesDisponiveis();
-        System.out.println("===== Acomodações disponíveis no hotel 2 =====");
-        hotel2.listarAcomodacoesDisponiveis();
-        System.out.println("===== Acomodações disponíveis no hotel 3 =====");
-        hotel3.listarAcomodacoesDisponiveis();
+        departamento3.adicionarFuncionario(new Analista("Claudio", "12985467125", LocalDate.now().minusYears(5), 5000.0, "teste", Analista.NivelSenioridade.SENIOR));
+        departamento3.adicionarFuncionario(new Gerente("Roberto", "237685263", LocalDate.now().minusYears(10), 7000.0, "teste", 750.0));
+        departamento3.adicionarFuncionario(new Vendedor("Gustavo", "1687561256", LocalDate.now().minusYears(1), 2000.0, 30.0, 4000.0));
 
-        redeDeHoteis.reservarAcomodacaoEspecifica(2143);
-        System.out.println("===== Acomodações disponíveis no hotel 3 =====");
-        hotel3.listarAcomodacoesDisponiveis();
-        System.out.println("===== Acomodações disponíveis na rede de hotéis =====");
-        redeDeHoteis.buscarAcomodacoesDisponiveis();
-        System.out.println("===== Cálculo de receita para 10 noites no hotel 1 =====");
-        System.out.println("R$ " + hotel1.calcularReceitaTotal(10));
+        empresa.adicionarDepartamentos(departamento1);
+        empresa.adicionarDepartamentos(departamento2);
+        empresa.adicionarDepartamentos(departamento3);
+
+        empresa.buscarFuncionarioPorNome("Claudio").forEach(System.out::println);
+        empresa.buscarFuncionarioPorNome("valdo").forEach(System.out::println);
+
+        System.out.println("Folha de pagamento total: R$" + empresa.calcularFolhaDePagamentoTotal());
+
+        System.out.println();
+        empresa.listarFuncionariosPorTempoServico().forEach(System.out::println);
     }
 }
--- Heranca-Polimorfismo-Java/src/main/java/atividade10/Empresa.java
+++ Heranca-Polimorfismo-Java/src/main/java/atividade10/Empresa.java
@@ -1,26 +1,27 @@
 package main.java.atividade10;
 
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class Empresa {
     private List<Departamento> listaDeDepartamentos;
+
+    public Empresa() {
+        this.listaDeDepartamentos = new ArrayList<>();
+    }
 
     public void adicionarDepartamentos(Departamento departamento) {
         this.listaDeDepartamentos.add(departamento);
     }
 
-    public Optional<Funcionario> buscarFuncionarioPorNome(String nome) {
-        return listaDeDepartamentos.stream().map(Departamento::getListaDeFuncionarios).flatMap(Collection::stream).filter(funcionario -> funcionario.getNome().equals(nome)).findFirst();
+    public List<Funcionario> buscarFuncionarioPorNome(String nome) {
+        return listaDeDepartamentos.stream().map(Departamento::getListaDeFuncionarios).flatMap(Collection::stream).filter(funcionario -> funcionario.getNome().contains(nome)).toList();
     }
 
     public Double calcularFolhaDePagamentoTotal() {
         return listaDeDepartamentos.stream().mapToDouble(Departamento::calcularFolhaDePagamento).sum();
     }
 
-    public void listarFuncionariosPorTempoServico() {
-        listaDeDepartamentos.stream().map(Departamento::getListaDeFuncionarios).flatMap(Collection::stream).max(Comparator.comparingInt(Funcionario::calcularTempoServico)).ifPresent(System.out::println);
+    public List<String> listarFuncionariosPorTempoServico() {
+        return listaDeDepartamentos.stream().map(Departamento::getListaDeFuncionarios).flatMap(Collection::stream).sorted((f1, f2) -> Integer.compare(f2.calcularTempoServico(), f1.calcularTempoServico())).map(Funcionario::toString).toList();
     }
 }
