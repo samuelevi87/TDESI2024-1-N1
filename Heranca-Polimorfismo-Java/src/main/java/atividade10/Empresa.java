1707868dd71c3d8d251bb93dfe35d17d4ee0d42f
GABRIELLA MAUREA BENTO <gabriella_bento@estudante.sesisenai.org.br>
2024-10-02 21:40:48 -0300

    Atividade 10: Correção métodos Empresa

--- Heranca-Polimorfismo-Java/src/main/java/atividade10/Empresa.java
+++ Heranca-Polimorfismo-Java/src/main/java/atividade10/Empresa.java
@@ -2,6 +2,7 @@ package main.java.atividade10;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Empresa {
     private String nome;
@@ -12,6 +13,36 @@ public class Empresa {
     }
     public void adicionarDepartamento(Departamento departamento){
         departamentos.add(departamento);
+    }
+
+    public List<Funcionario> buscarFuncionarioPorNome(String nome){
+        List<Funcionario> funcionariosBuscados = new ArrayList<>();
+
+        for(Departamento departamento : departamentos){
+            for(Funcionario funcionario : departamento.getFuncionarios()){
+                if(funcionario.getNome().toLowerCase().contains(nome.toLowerCase())){
+                     funcionariosBuscados.add(funcionario);
+                }
+            }
+        }
+        return funcionariosBuscados;
+    }
+    public double calcularFolhaPagamentoTotal(){
+        double total = 0.0;
+        for(Departamento departamento : departamentos){
+            total += departamento.calcularFolhaPagamento();
+        }
+        return total;
+    }
+    public String gerarRelatorioTempoDeServico(){
+        StringBuilder relatorio = new StringBuilder("Relatório de Tempo de Serviço:\n");
+        List<Funcionario> funcionarios = new ArrayList<>();
+
+        for(Departamento departamento : departamentos){
+            funcionarios.addAll(departamento.getFuncionarios());
+        }
+       return relatorio.toString();
+
     }
 
 }
